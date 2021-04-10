cce6d98bf8b761dcd611fce77d8b7bf3d853e752
Evrice Network <official@evrice.com>
2021-04-10 18:24:45 +0400

    Update hd_test.go

--- accounts/hd_test.go
+++ accounts/hd_test.go
@@ -34,15 +34,15 @@ func TestHDPathParsing(t *testing.T) {
 		{"m/44'/1020'/0'/128", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 128}},
 		{"m/44'/1020'/0'/0'", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0x80000000 + 0}},
 		{"m/44'/1020'/0'/128'", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0x80000000 + 128}},
-		{"m/2147483692/2147483708/2147482688/0", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0}},
-		{"m/2147483692/2147483708/2147482688/2147482688", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0x80000000 + 0}},
+		{"m/2147483692/2147484668/2147483648/0", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0}},
+		{"m/2147483692/2147484668/2147483648/2147483648", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0x80000000 + 0}},
 
 		// Plain relative derivation paths
 		{"0", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0, 0}},
 		{"128", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0, 128}},
 		{"0'", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0, 0x80000000 + 0}},
 		{"128'", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0, 0x80000000 + 128}},
-		{"2147482688", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0, 0x80000000 + 0}},
+		{"2147483648", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0, 0x80000000 + 0}},
 
 		// Hexadecimal absolute derivation paths
 		{"m/0x2C'/0x3fc'/0x00'/0x00", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0}},
@@ -60,14 +60,14 @@ func TestHDPathParsing(t *testing.T) {
 		{"0x80000000", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0, 0x80000000 + 0}},
 
 		// Weird inputs just to ensure they work
-		{"	m  /   44			'\n/\n   60	\n\n\t'   /\n0 ' /\t\t	0", DerivationPath{0x80000000 + 44, 0x80000000 + 60, 0x80000000 + 0, 0}},
+		{"	m  /   44			'\n/\n   60	\n\n\t'   /\n0 ' /\t\t	0", DerivationPath{0x80000000 + 44, 0x80000000 + 1020, 0x80000000 + 0, 0}},
 
 		// Invalid derivation paths
 		{"", nil},              // Empty relative derivation path
 		{"m", nil},             // Empty absolute derivation path
 		{"m/", nil},            // Missing last derivation component
 		{"/44'/1020'/0'/0", nil}, // Absolute path without m prefix, might be user error
-		{"m/2147482688'", nil}, // Overflows 32 bit integer
+		{"m/2147483648'", nil}, // Overflows 32 bit integer
 		{"m/-1'", nil},         // Cannot contain negative number
 	}
 	for i, tt := range tests {
